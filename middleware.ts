c4660d80548cfb1ecfddd3342c77fbd6fc5f1e7a
RME Voyage Design Agent <design-agent@rme-voyage.local>
2026-09-11 02:37:24 +0000

    design: fondations du design system (tokens Tailwind, typographie Boska/General Sans, CSP Fontshare)

--- middleware.ts
+++ middleware.ts
@@ -63,8 +63,8 @@ export function middleware(request: NextRequest) {
   const csp = [
     "default-src 'self'",
     "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://api.aladhan.com",
-    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com",
-    "font-src 'self' https://fonts.gstatic.com https://unpkg.com",
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com https://api.fontshare.com https://cdn.fontshare.com",
+    "font-src 'self' https://fonts.gstatic.com https://unpkg.com https://cdn.fontshare.com",
     "img-src 'self' data: https: blob:",
     "connect-src 'self' https://api.aladhan.com https://*.tile.openstreetmap.org https://router.project-osrm.org",
     "frame-src 'self' https://www.openstreetmap.org",
